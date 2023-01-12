2969b7ea2762bee0c43a3257fc6bc1611c421d38
Juanicamuss21 <juanicamuss15@gmail.com>
2023-01-12 12:54:19 -0300

    deploy

--- api/index.js
+++ api/index.js
@@ -19,12 +19,12 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const PORT = process.env.PORT || 3001
+const port = process.env.PORT || 3001
 
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(PORT, () => {
-    console.log('%s listening at', PORT); // eslint-disable-line no-console
+  server.listen(port, () => {
+    console.log('%s listening at', port); // eslint-disable-line no-console
   });
 });
